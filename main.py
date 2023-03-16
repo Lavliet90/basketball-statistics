66ced84934bc00664f0a24bd26866ab1d12360ff
Lavliet <bbb.zzzz.bbb@mail.ru>
2023-03-16 16:51:43 +0100

    optimization team_name()

--- main.py
+++ main.py
@@ -5,29 +5,24 @@ import requests
 def team_name():
     basket = requests.get("https://www.balldontlie.io/api/v1/teams").json()
     list_commands = basket.get('data')
-    region_list = {'Southeast': [], 'Southwest': [], 'Atlantic': [], 'Central': [], 'Northwest': [], 'Pacific': []}
+    region_list = {
+        'Southeast': [],
+        'Southwest': [],
+        'Atlantic': [],
+        'Central': [],
+        'Northwest': [],
+        'Pacific': []
+    }
 
     for list_command in list_commands:
-        if list_command.get('division') == 'Southeast':
-            region_list.get('Southeast').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        elif list_command.get('division') == 'Atlantic':
-            region_list.get('Atlantic').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        elif list_command.get('division') == 'Central':
-            region_list.get('Central').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        elif list_command.get('division') == 'Northwest':
-            region_list.get('Northwest').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        elif list_command.get('division') == 'Pacific':
-            region_list.get('Pacific').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        elif list_command.get('division') == 'Southwest':
-            region_list.get('Southwest').append(
-                (list_command.get('full_name')) + ' (' + (list_command.get('abbreviation')) + ')')
-        else:
-            print('Error from API')
+        name_team_and_tag = '{} ({})'.format(
+            list_command.get('full_name'),
+            list_command.get('abbreviation')
+        )
+        devision_team = list_command.get('division')
+        region_list.get(
+            devision_team, "Error from API"
+        ).append(name_team_and_tag)
 
     for region in region_list:
         click.echo(click.style(region, fg='green'))
